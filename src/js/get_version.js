45d37750de0d6b1aa99a7fe86fe836f6c6b2ec3a
Trevor Covington <tcovington189@gmail.com>
2017-02-10 23:29:05 -0600

    Redefining styles, finializing home page css and js

--- src/js/get_version.js
+++ src/js/get_version.js
@@ -2,12 +2,9 @@
 
 // User picks version of game they are playing and set localStorage accordingly
 
-$(".version_X").on("click", function(){
-    localStorage.setItem("version","X");
-    localStorage.setItem("location", "Vaniville_Town"); 
-});
-
-$(".version_Y").on("click", function(){
-    localStorage.setItem("version","Y");
-    localStorage.setItem("location", "Vaniville_Town");
+$('#version_button_group .button').on('click', function(){
+    var version = $(this).attr('id');
+    localStorage.setItem("version", version.toUpperCase());
+    
+    window.location.href = 'tracker.html';
 });
