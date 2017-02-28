1e8fa676ae794aa3b5b04dc0fb8e97b75d2434da
tcov189 <tcovington189@gmail.com>
2017-02-28 14:06:51 -0600

    data changes

--- src/js/get_version.js
+++ src/js/get_version.js
@@ -6,12 +6,15 @@ $('#version_button_group .button').on('click', function(){
     var version = $(this).attr('id');    
     var name = $('#version_button_group .button#'+ localStorage.getItem('version')).attr('name');
     
-    if (localStorage.getItem('version') != null && localStorage.getItem('version') !== version) {
-        
-        if (confirm('It looks like you already started a game in ' + name + '. Would you like to start a new one?') == true) {
-            localStorage.clear();
-            localStorage.setItem("version", version);
-        } 
+    if (localStorage.getItem('version') != null) {
+        if (localStorage.getItem('version') !== version) {
+             if (confirm('It looks like you already started a game in ' + name + '. Would you like to start a new one?') == true) {
+                localStorage.clear();
+                localStorage.setItem("version", version);
+            }    
+        }        
+    } else {
+        localStorage.setItem("version", version);
     }    
     
     window.location.href = 'tracker.html';
