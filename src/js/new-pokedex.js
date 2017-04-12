897e82e0ff58df893fc5d097b83db564e4d58b25
tcov189 <tcovington189@gmail.com>
2017-04-12 17:17:25 -0500

    Moved to new sprite with updated images, created page to help with making sure new sprites lined up, created mixin to make writing bg positions easier

--- src/js/new-pokedex.js
+++ src/js/new-pokedex.js
@@ -11,8 +11,23 @@ function compare(a,b) {
   return 0;
 }
 
+//Load national dex into localstorage if not there already
+if (!localStorage.getItem('national_dex')) {
+   //Get National Dex
+    $.ajax({
+        url: 'data/pokedexes/national-dex-post-6.json',
+        async: false,
+        success: function(data){
+            var data = JSON.stringify(data);
+            localStorage.setItem('national_dex', data);            
+        }
+    });    
+}
+
+nationalDexData = JSON.parse(localStorage.getItem('national_dex'));
+
 //Sort the pokedexArr by dex number
-pokedexArr.sort(compare);
+
 
 
 //Creating the dom and adding functionality
@@ -45,20 +60,20 @@ $("document").ready(function(){
   
     //Creating Pokemon cards
     //Loop through every Pokemon obj in the pokedexArr
-    for (var i = 0; i < pokedexArr.length; i++){                            
+    for (var i = 0; i < nationalDexData.length; i++){                            
 
             //Check to see if current Pokemon obj is in localStorage
-            var wasPokemonCaught = localStorage.getItem(pokedexArr[i].name); 
+            var wasPokemonCaught = localStorage.getItem(nationalDexData[i].name); 
 
             //Start of the Pokemon Card div        
-            theHTML = '<div class="pkmn-card" id="' + pokedexArr[i].name + '"><div class="pkmn-card-info">';
+            theHTML = '<div class="pkmn-card" id="' + nationalDexData[i].name + '"><div class="pkmn-card-info">';
             
             theHTML += '<div class="pkmn-catch-icon">';
             //Determine if Pokemon has already been caught and set pokeball icon accordingly
-            if (wasPokemonCaught === 'Caught' || pokedexArr[i].caught === 'Yes'){
-              theHTML += '<img class="'+ pokedexArr[i].dexNum +' catch" src="images/pokeball-catch.png">';
+            if (wasPokemonCaught === 'Caught' || nationalDexData[i].caught === 'Yes'){
+              theHTML += '<img class="'+ nationalDexData[i].dexNum +' catch" src="images/pokeball-catch.png">';
             } else {
-              theHTML += '<img class="'+ pokedexArr[i].dexNum +' no-catch" src="images/pokeball-no-catch.png">';
+              theHTML += '<img class="'+ nationalDexData[i].dexNum +' no-catch" src="images/pokeball-no-catch.png">';
             }
         
             theHTML += '</div>';
@@ -66,15 +81,20 @@ $("document").ready(function(){
             theHTML += '<div class="pkmn-data">';
 
             //Add more Pokemon object info to html
-            theHTML += '<i class="sprites '+ pokedexArr[i].name +' "></i>'+
-                '<a href="" onclick="toPokeInfo('+ pokedexArr[i].name +');">' + pokedexArr[i].name +' #'+ pokedexArr[i].dexNum + '</a>';               
+            theHTML += '<i class="sprites '+ nationalDexData[i].name +' "></i>';
+            if (nationalDexData[i].forms) {
+                $.each(nationalDexData[i].forms, function(index, val){
+                   theHTML += '<i class="sprites '+ nationalDexData[i].name +' '+ val.name +'"></i>';
+                }) 
+            }
+                theHTML += '<a href="" onclick="toPokeInfo('+ nationalDexData[i].name +');">' + nationalDexData[i].name +' #'+ nationalDexData[i].dexNum + '</a>';               
 
             //Add types to Pokemon card
-            theHTML += '<div class="types"><i class="type-icon '+ pokedexArr[i].type[0] +'"></i>';
+            theHTML += '<div class="types"><i class="type-icon '+ nationalDexData[i].type[0] +'"></i>';
         
             //If Pokemon obj has more than one type, display the second type
-            if (pokedexArr[i].type[1] !== null){
-                theHTML += '&nbsp;|&nbsp;<i class="type-icon '+ pokedexArr[i].type[1] +'"></i>';
+            if (nationalDexData[i].type[1] !== null){
+                theHTML += '&nbsp;|&nbsp;<i class="type-icon '+ nationalDexData[i].type[1] +'"></i>';
             }                                                                       
 
             //Add the EV yields to the Pokemon Card
@@ -82,6 +102,6 @@ $("document").ready(function(){
 
         
         $dexDiv.append(theHTML);     
-        toggleCatch(pokedexArr[i]);
+        toggleCatch(nationalDexData[i]);
     }
 });
