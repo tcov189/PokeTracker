bfb9a4e38afcc1e285bb850d602fc90c3931a2ed
Trevor Covington <tcovington189@gmail.com>
2017-05-05 19:49:45 -0500

    Don't need src files

--- src/js/dropdowns.js
+++
@@ -1,7 +0,0 @@
-//------- Functionality for home page --//
-
-//--- Dropdowns
-$('.dropdown-heading').on('click', function(){
-    $(this).siblings().slideToggle();
-    $(this).toggleClass('open');
-});
