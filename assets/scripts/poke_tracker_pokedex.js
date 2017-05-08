f17f0ac99795b97b38c187f8b34d1970ca88165e
Trevor Covington <tcovington189@gmail.com>
2017-05-08 16:47:14 -0500

    JS for pokedex

--- assets/scripts/poke_tracker_pokedex.js
+++ assets/scripts/poke_tracker_pokedex.js
@@ -7,20 +7,69 @@ void 0!==c?e&&"set"in e&&void 0!==(d=e.set(a,c,b))?d:a[b]=c:e&&"get"in e&&null!=
 
 //Vars needed
 var pokedexDiv = $('#pokedex');
+var regionalDexData     = JSON.parse(localStorage.getItem('regional_dex'));
+var nationalDexData     = JSON.parse(localStorage.getItem('national_dex'));
 
+var caughtPokemonArray  = JSON.parse(localStorage.getItem('pokemon_caught'));
+
+// If there is no national dex in localStorage, then just default to Gen 7
+if (!nationalDexData) {
+    //Get National Dex
+    $.ajax({
+        url: 'assets/data/pokedexes/national-dex-post-6.json',
+        async: false,
+        success: function(data){
+            data = JSON.stringify(data);
+            localStorage.setItem('national_dex', data);
+            nationalDexData = JSON.parse(localStorage.getItem('national_dex'));
+        }
+    });
+}
+
+// If there is no caught pokemon, create the array
+if (!caughtPokemonArray){
+    var pkmnCaughtArr = [];
+    localStorage.setItem('pokemon_caught', JSON.stringify(pkmnCaughtArr));
+} else {
+    pkmnCaughtArr  = JSON.parse(localStorage.getItem('pokemon_caught'));
+} 
+    
 //Function for generating html
-var generatePokedex = function () {
+var generatePokedexCards = function (pokemon) {
     'use strict';
     
-    var theHtml = '<div class="card card-default card_pokemon">';
-        
-        
-    theHtml += '</div>';//End card div        
+    var theHtml = '<div class="card card-default card_pokemon">' +        
+                    '<div class="card_pokemon-pokeball">' +
+                        '<i class="card_pokemon-pokeball-icon" id="'+ pokemon.name +'"></i>' +
+                    '</div>' + // End card_pokemon-pokeball
+                    '<div class="card_pokemon-info">' +
+                        '<div class="pokemon-info-bio">' +
+                            '<div class="pokemon-info-bio-name-sprite">' +
+                                '<i class="sprites '+ pokemon.name +'"></i>' +
+                                '<span class="pokemon-bio-name">'+ pokemon.name +'</span>' +
+                            '</div>' + //end pokemon-info-bio-name-sprite
+                            '<div class="pokemon-bio-types">';
+                                $.each(pokemon.type, function (i, type){
+                                       theHtml += '<i class="type-icon '+ type.toLowerCase() +' "></i>'; 
+                                   });
+                 theHtml += '</div>' + //end pokemon-bio-types
+                        '</div>' +// end card_pokemon-info-bio
+                    '</div>' + // end card_pokemon-info        
+                '</div>';//End card div        
     
     return theHtml;
 };
 
 
-pokedexDiv.append(generatePokedex());
+//Function for creating all the pokedex divs
+var generatePokedex = function () {    
+    // Var for the pokedex Html
+    var pokedexHtml= '';
+    
+    for (var i = 0; i < nationalDexData.length; i++) {        
+        pokedexHtml += generatePokedexCards(nationalDexData[i]);
+    }   
+    return pokedexHtml;
+};
 
-/*<div class="card card-default card_pokemon "><div class="card_pokemon-pokeball"><i class="card_pokemon-pokeball-icon" id="caterpie"></i></div><div class="card_pokemon-info"><div class="pokemon-info-bio"><div class="pokemon-info-bio-name-sprite"><i class="sprites caterpie undefined"></i><span class="pokemon-bio-name">caterpie</span></div><div class="pokemon-bio-types"><i class="type-icon bug "></i></div></div><hr><div class="pokemon-info-rates"><div class="pokemon-rate-block"><span><strong>grass</strong>: 20%</span></div><div class="pokemon-sos-block"><div class="text-center">S.O.S:</div><i class="sprites caterpie undefined"></i></div><div class="pokemon-levels-block"><span class="level-range">Lvl: 2–3</span></div></div></div></div>*/
+pokedexDiv.html(generatePokedex());
